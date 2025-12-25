67a74b0ee3e533c7c47d8f25199274233ba4222c
Mahdi Jenhani <mahdijenhani00@gmail.com>
2025-12-25 06:43:35 +0100

    fix websocket allowed origins

--- backend/src/main/java/com/eventhub/config/WebSocketConfig.java
+++ backend/src/main/java/com/eventhub/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://eventhub-oj8j.onrender.com")
+                .setAllowedOrigins("https://eventhub-bli3.onrender.com")
                 .withSockJS();
     }
 }
