b561dad1def83363cbd6700674903ed70b17ea3d
Mahdi Jenhani <mahdijenhani00@gmail.com>
2025-12-28 10:14:20 +0100

    Fix user message

--- frontend/src/app/services/backend-status.service.ts
+++ frontend/src/app/services/backend-status.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, interval, of } from 'rxjs';
-import { catchError, switchMap, startWith } from 'rxjs/operators';
+import { catchError, switchMap, startWith, timeout, finalize } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class BackendStatusService {
   private backendActiveSubject = new BehaviorSubject<boolean>(true);
   backendActive$ = this.backendActiveSubject.asObservable();
+  private checking = false;
 
   constructor(private http: HttpClient) {
     interval(4000)
+      .pipe(startWith(0))
+      .subscribe(() => {
+        this.checkBackend();
+      });
+  }
+
+  checkBackend() {
+    // Show spinner while checking
+    this.backendActiveSubject.next(false);
+    this.http.get(environment.apiUrl + '/health', { responseType: 'text' })
       .pipe(
-        startWith(0),
-        switchMap(() =>
-          this.http.get(environment.apiUrl + '/health', { responseType: 'text' }).pipe(
-            catchError(() => of(null))
-          )
-        )
+        timeout(10000), // 10 seconds timeout
+        catchError(() => of(null)),
+        finalize(() => {})
       )
       .subscribe((result) => {
         this.backendActiveSubject.next(!!result);
