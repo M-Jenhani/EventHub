ca81020f7249ba82a5d6cc4dbd4132d7facb3a7c
Mahdi Jenhani <mahdijenhani00@gmail.com>
2025-12-28 10:26:42 +0100

    Fix user message

--- frontend/src/app/components/backend-wakeup-message/backend-wakeup-message.component.ts
+++ frontend/src/app/components/backend-wakeup-message/backend-wakeup-message.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-backend-wakeup-message',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './backend-wakeup-message.component.html',
   styleUrls: ['./backend-wakeup-message.component.scss']
 })
